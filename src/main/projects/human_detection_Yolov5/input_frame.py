a51f86e22624839b4ac676b0da3f6e393b67775e
kimlolipop <nrtp.dev@gmail.com>
2021-12-20 00:18:32 +0700

    test

--- src/main/projects/human_detection_Yolov5/input_frame.py
+++ src/main/projects/human_detection_Yolov5/input_frame.py
@@ -9,7 +9,7 @@ import numpy as np
 import torch
 import pandas as pd
 import av
-import dlib
+# import dlib
 
 from src.main.projects.human_detection_Yolov5.sort import *
 try:
@@ -34,7 +34,7 @@ mot_tracker = Sort() ## --> realtime tracker
 # motion detector
 bgsub = cv2.createBackgroundSubtractorKNN(10) 
 # face_detector
-face_detector = dlib.get_frontal_face_detector()
+# face_detector = dlib.get_frontal_face_detector()
 # sp = dlib.shape_predictor("./src/main/projects/human_detection_Yolov5/model/shape_predictor_68_face_landmarks.dat")
 
 # Setting for online connect
@@ -65,9 +65,9 @@ def webcam_input(option):
             elif self.type == "Subtraction":
                 img = cv2.GaussianBlur(img,(5,5),0)
                 img = cv2.cvtColor(Subtraction(img),cv2.COLOR_GRAY2BGR)
-            elif self.type == "Face_Detector":
-                img = cv2.GaussianBlur(img,(5,5),0)
-                img = face_detection(img)
+#             elif self.type == "Face_Detector":
+#                 img = cv2.GaussianBlur(img,(5,5),0)
+#                 img = face_detection(img)
                 
                         
             return av.VideoFrame.from_ndarray(img, format="bgr24")
